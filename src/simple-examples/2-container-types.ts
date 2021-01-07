f3c0001ebee0f3a939af57498db731d6a74d6231
Jacek Kopecky <jacek@jacek.cz>
2021-01-07 11:45:34 +0000

    more changes from review

--- src/simple-examples/2-container-types.ts
+++ src/simple-examples/2-container-types.ts
@@ -1,10 +1,10 @@
 let names: string[];
-let rgbColour: [number, number, number];
+let rgb: [number, number, number];
 let droid: object;
 
 names = ['Matt', 'Rich', 'Jack'];
 
-rgbColour = [255, 0, 0];
+rgb = [255, 0, 0];
 
 droid = {
   name: 'C3-PO',
--- src/simple-examples/4-optional-readonly.ts
+++ src/simple-examples/4-optional-readonly.ts
@@ -8,3 +8,12 @@ interface ContactToCall {
   readonly num: string,
   readonly name: string,
 }
+
+
+function callContact(c: ContactToCall) {
+  if (c.num == '112') {
+    // emergency call
+  } else {
+    // normal call
+  }
+}
